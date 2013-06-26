7274c1a50125409d06fbbdb0d6204db7a193f5ce
Jon Linklater-Johnson <jon@binarytal.es>
2013-06-26 11:21:30 +0100

    work as of morning of 26th June for Heroke test

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var http = require('http'),
     path = require('path'),
     hbs = require ('hbs'),
+    sass = require('node-sass'),
     express = require('express');
 
 var app = express();
@@ -14,8 +15,15 @@ app.configure(function(){
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
+  app.use(sass.middleware({
+     src: __dirname + '/public/sass',
+     dest: __dirname + '/public',
+     debug: true
+  }));
   app.use(express.static(path.join(__dirname, 'public')));
 });
+
+hbs.registerPartials(__dirname + '/views/partials');
 
 app.configure('development', function(){
   app.use(express.errorHandler());
